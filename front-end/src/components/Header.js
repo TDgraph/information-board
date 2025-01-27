70dfc2b5a4eec72ac7aac27cf7fcedc390933e77
Temitope Douglas <taymeedouglas07@gmail.com>
2025-01-27 05:06:10 +0100

    updsated

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,4 +1,4 @@
-// src/components/Header.js
+
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -1,4 +1,4 @@
-// src/components/Signup.js
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
